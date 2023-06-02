68009677f7a040b603ab1124f830b9b34d8c4f58
Licks-creat <2020ugme089@nitjsr.ac.in>
2023-06-03 03:30:06 +0530

    new2

--- FrontEnd/Quiz-07.js
+++ FrontEnd/Quiz-07.js
@@ -5,5 +5,5 @@ students.forEach(function (element){
     console.log(element.name," uses ",this.b)
  },{b:"LinkedIn"})
  
- 1)"Vivek uses Twitter"  2)"Vivek uses LinkedIn"
-   "Rahul uses Twitter"    "Rahul uses LinkedIn"
+ 1)"Vivek uses Twitter"   2)"Vivek uses LinkedIn"
+   "Rahul uses Twitter"     "Rahul uses LinkedIn"
